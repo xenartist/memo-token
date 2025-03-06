27ecb53b30e2d12f85addc20ea58ba43c10d319a
xenartist <146194585+xenartist@users.noreply.github.com>
2025-03-06 15:34:58 +1100

    update with token 2022

--- programs/memo-token/src/lib.rs
+++ programs/memo-token/src/lib.rs
@@ -1,5 +1,8 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Mint, Token, TokenAccount};
+use anchor_spl::{
+    token_2022::{self, Token2022},
+    token_interface::{Mint, TokenAccount},
+};
 
 declare_id!("68ASgTRCbbwsfgvpkfp3LvdXbpn33QbxbV64jXVaW8Ap");
 
@@ -26,10 +29,10 @@ pub mod memo_token {
             &[bump],
         ];
         
-        token::mint_to(
+        token_2022::mint_to(
             CpiContext::new_with_signer(
                 ctx.accounts.token_program.to_account_info(),
-                token::MintTo {
+                token_2022::MintTo {
                     mint: ctx.accounts.mint.to_account_info(),
                     to: ctx.accounts.token_account.to_account_info(),
                     authority: ctx.accounts.mint_authority.to_account_info(),
@@ -49,13 +52,15 @@ pub struct ProcessTransfer<'info> {
     pub user: Signer<'info>,
     
     #[account(mut)]
-    pub mint: Account<'info, Mint>,
+    /// Interface for the SPL Token 2022 Mint account
+    pub mint: InterfaceAccount<'info, Mint>,
     
     /// CHECK: PDA as mint authority
     pub mint_authority: AccountInfo<'info>,
     
     #[account(mut)]
-    pub token_account: Account<'info, TokenAccount>,
+    /// Interface for the SPL Token 2022 Token account
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
     
-    pub token_program: Program<'info, Token>,
+    pub token_program: Program<'info, Token2022>,
 }
